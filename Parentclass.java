513b08ea30802614bc21d9d296710d31e6a45b14
Aryan meshram <122171960+aryan0142@users.noreply.github.com>
2023-03-27 13:45:08 +0530

    Create Parentclass.java

---
+++ Parentclass.java
@@ -0,0 +1,32 @@
+class Animal {
+  void eat() {
+    System.out.println("Eating...");
+  }
+}
+
+
+class Cat extends Animal {
+  void meow() {
+    System.out.println("Meowing...");
+  }
+}
+
+
+class Dog extends Animal {
+  void bark() {
+    System.out.println("Barking...");
+  }
+}
+
+
+public class Main {
+  public static void main(String[] args) {
+    Cat myCat = new Cat();
+    myCat.eat(); // Inherited method from Animal class
+    myCat.meow(); // Method specific to Cat class
+
+    Dog myDog = new Dog();
+    myDog.eat(); // Inherited method from Animal class
+    myDog.bark(); // Method specific to Dog class
+  }
+}
